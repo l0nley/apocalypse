9ea53710342baca8a6e8324adaf0d32728faa958
Vladimir <hofvl0nley@gmail.com>
2013-11-15 14:34:58 +0300

    Added batch command processing for console

--- BinaryProtocolConsole/Program.cs
+++ BinaryProtocolConsole/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 
@@ -11,19 +12,37 @@ namespace BinaryProtocolConsole
 
         private static Dictionary<string,ConsoleCommand> _commands = new Dictionary<string, ConsoleCommand>();
 
-        static void Main()
+        static int  Main(string[] args)
         {
             string s;
+            Environment.Add("breakScriptAfterError",true);
+            Environment.Add("autoExitAfterExecutingSuccess",false);
             Console.BackgroundColor = ConsoleColor.Blue;
             Console.ForegroundColor = ConsoleColor.White;
             Console.Clear();
             Console.WriteLine("Loading commands...");
             _commands = Assembly.GetExecutingAssembly()
                                 .GetTypes()
-                                .Where(l => l.IsSubclassOf(typeof (ConsoleCommand)))
-                                .Select(l => (ConsoleCommand) Activator.CreateInstance(l)).ToDictionary(l => l.Name);
+                                .Where(l => l.IsSubclassOf(typeof(ConsoleCommand)))
+                                .Select(l => (ConsoleCommand)Activator.CreateInstance(l)).ToDictionary(l => l.Name);
             Console.WriteLine("Loading commands complete.");
             Console.WriteLine("Welcome to console master");
+
+            if (args.Length != 0)
+            {
+                var fileName = args[0];
+                int code;
+                if ((code=ExecuteFile(fileName)) != 0)
+                {
+                    return code;
+                }
+                var exitAfterSuccess = (bool)Environment["autoExitAfterExecutingSuccess"];
+                if (exitAfterSuccess)
+                {
+                    return 0;
+                }
+            }
+           
             Console.Write(Prompt);
             while ("exit" != (s = Console.ReadLine()))
             {
@@ -62,6 +81,65 @@ namespace BinaryProtocolConsole
                 }
                 Console.Write(Prompt);
             }
+
+            return 0;
+        }
+
+        private static int ExecuteFile(string fileName)
+        {
+            var breakAfterError = (bool) Environment["breakScriptAfterError"];
+            try
+            {
+                string[] commands;
+                using (TextReader fs = File.OpenText(fileName))
+                {
+                    commands = fs.ReadToEnd().Split(new [] {"\n"},StringSplitOptions.RemoveEmptyEntries);
+                }
+                foreach (var command in commands)
+                {
+                    var ss = command.Split(' ');
+                    var ccmd = ss[0];
+                    var @params = ss.Skip(1).ToArray();
+                    if (_commands.ContainsKey(ccmd))
+                    {
+                        var ocmd = _commands[ccmd];
+                        if (@params.Length < ocmd.ArgsCount)
+                        {
+                            Console.WriteLine("Not enough params for command {0}",ccmd);
+                            if (breakAfterError)
+                            {
+                                return 3;
+                            }
+                            continue;
+                        }
+                        try
+                        {
+                            ocmd.ExecuteCommand(@params,Environment);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("Error in command {0}\n{1}\n{2}",ccmd,e.Message,e.StackTrace);
+                            if (breakAfterError)
+                            {
+                                return 4;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (breakAfterError)
+                        {
+                            return 2;
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error when executing file \n{0}\n{1}",e.Message,e.StackTrace);
+                return 1;
+            }
+            return 0;
         }
 
         static string Prompt
